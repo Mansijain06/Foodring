c092a25fa6b17dbd5fcc82c54f0134e9c04cf093
Mansi Jain <70169492+Mansijain06@users.noreply.github.com>
2023-02-23 23:06:13 +0530

    fix url

--- Foodring/src/config.js
+++ Foodring/src/config.js
@@ -1,8 +1,8 @@
 export const RESTAURANTS_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.73651&lng=75.889219&page_type=DESKTOP_WEB_LISTING";
+  "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
 
-export const RESTAURANT_OFFER_URL = "https://www.swiggy.com/dapi/offers/restaurant?lat=22.73651&lng=75.889219&offset=0"
+export const RESTAURANT_OFFER_URL = "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/offers/restaurant?lat=22.73651&lng=75.889219&offset=0"
 
 export const RESTAURANTS_IMG_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
